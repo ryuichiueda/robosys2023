7997a78a3e10259e82f97bd4020064452cbdc194
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-10-26 12:44:37 +0900

    Make test human friendly

--- test.bash
+++ test.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-out=$(seq 5 | ./plus)
+ng () {
+	echo NG at Line ${1}
+	ret=1
+}
 
-[ "${out}" = 15 ]
+ret=0
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "${LINENO}"
+
+[ "$ret" = 0 ] && echo OK
+exit $ret
