07100bcecc679f10dba045e954f9b6aaf5931e2c
Ryuichi Ueda <ryuichiueda@gmail.com>
2023-10-26 12:46:41 +0900

    Add test cases

--- test.bash
+++ test.bash
@@ -9,5 +9,14 @@ ret=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "${LINENO}"
 
+### 変なinput ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "${LINENO}"
+[ "${out}" = "" ] || ng "${LINENO}"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "${LINENO}"
+[ "${out}" = "" ] || ng "${LINENO}"
+
 [ "$ret" = 0 ] && echo OK
 exit $ret
